738517dad09337fa221a8f081956f239b8bb5c43
Salvador Jacobi <salvador.jacobi@gmail.com>
2012-01-06 16:29:44 +0100

    Add titles to preview and hold

--- src/main/java/com/salvadorjacobi/tetris/HoldView.java
+++ src/main/java/com/salvadorjacobi/tetris/HoldView.java
@@ -1,7 +1,11 @@
 package com.salvadorjacobi.tetris;
 
+import java.awt.Color;
 import java.util.Observable;
 import java.util.Observer;
+
+import javax.swing.BorderFactory;
+import javax.swing.border.TitledBorder;
 
 public class HoldView extends TetriminoView implements Observer {
 	private final GameModel model;
@@ -10,6 +14,16 @@ public class HoldView extends TetriminoView implements Observer {
 		super(model);
 
 		this.model = model;
+
+		this.setBorder(
+				BorderFactory.createTitledBorder(
+						BorderFactory.createEmptyBorder(),
+						"HOLD",
+						TitledBorder.CENTER,
+						TitledBorder.TOP,
+						null,
+						Color.WHITE)
+				);
 	}
 
 	public void update(Observable o, Object arg) {
