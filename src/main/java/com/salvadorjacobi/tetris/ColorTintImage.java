b5e9d2bd8e3d6bf49b443638a33e50b84be79f92
Salvador Jacobi <salvador.jacobi@gmail.com>
2012-01-12 16:54:04 +0100

    Tint image based on mixValue

--- src/main/java/com/salvadorjacobi/tetris/ColorTintImage.java
+++ src/main/java/com/salvadorjacobi/tetris/ColorTintImage.java
@@ -24,10 +24,25 @@ public class ColorTintImage implements BufferedImageOp {
 	public BufferedImage filter(BufferedImage src, BufferedImage dest) {
 		dest = new BufferedImage(src.getWidth(), src.getHeight(), src.getType());
 
-		for (int x = 0; x < dest.getWidth(); x++) {
-			for (int y = 0; y < dest.getHeight(); y++) {
-				int rgb = dest.getRGB(x, y);
-				dest.setRGB(x, y, rgb | tintColor.getRGB());
+		for (int x = 0; x < src.getWidth(); x++) {
+			for (int y = 0; y < src.getHeight(); y++) {
+				Color color = new Color(src.getRGB(x, y));
+
+				int r1 = color.getRed();
+				int r2 = tintColor.getRed();
+				int r3 = (int) (r1 + (r2 - r1) * mixRatio);
+
+				int g1 = color.getGreen();
+				int g2 = tintColor.getGreen();
+				int g3 = (int) (g1 + (g2 - g1) * mixRatio);
+
+				int b1 = color.getBlue();
+				int b2 = tintColor.getBlue();
+				int b3 = (int) (b1 + (b2 - b1) * mixRatio);
+
+				Color newColor = new Color(r3, g3, b3);
+
+				dest.setRGB(x, y, newColor.getRGB());
 			}
 		}
 
