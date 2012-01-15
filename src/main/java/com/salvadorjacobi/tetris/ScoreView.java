27add82131d1e7b60856f7e9eda9661953fc5951
Salvador Jacobi <salvador.jacobi@gmail.com>
2012-01-15 16:07:45 +0100

    Change ScoreView from JLabel to JPanel

--- src/main/java/com/salvadorjacobi/tetris/ScoreView.java
+++ src/main/java/com/salvadorjacobi/tetris/ScoreView.java
@@ -2,46 +2,74 @@ package com.salvadorjacobi.tetris;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class ScoreView extends JLabel implements Observer {
-	private static final String FORMAT = "<html><div align=\"right\">" +
-			"LEVEL<br>" +
-			"%d<br><br>" +
-			"SCORE<br>" +
-			"%d<br><br>" +
-			"</div></html>";
-
+public class ScoreView extends JPanel implements Observer {
 	private final GameModel model;
-	private final int scale;
+	private int levelWidth;
+	private int scoreWidth;
 
 	public ScoreView(GameModel model) {
 		super();
 
 		this.model = model;
-		scale = model.scale / 2;
 
+		int scale = model.scale / 2;
 		Dimension dimension = new Dimension(scale * 5, scale * 5);
 
 		setMinimumSize(dimension);
 		setMaximumSize(dimension);
 		setPreferredSize(dimension);
 
-		setBackground(Color.DARK_GRAY);
-		setForeground(Color.WHITE);
+		int levelWidth = getFontMetrics(Constants.baseFont).stringWidth("LEVEL");
+		int scoreWidth = getFontMetrics(Constants.baseFont).stringWidth("SCORE");
+	}
 
-		setFont(Constants.baseFont);
+	@Override
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 
-		update(model, null);
-		setOpaque(true);
+		Graphics2D g2d = (Graphics2D) g;
+
+		// Background
+		g2d.setBackground(Color.DARK_GRAY);
+		g2d.clearRect(0, 0, getWidth(), getHeight());
+
+		g2d.setFont(Constants.baseFont);
+		g2d.setColor(Color.WHITE);
+
+		String level = Integer.toString(model.getLevel());
+		String score = Integer.toString(model.getScore());
+
+		int maxAscent = g2d.getFontMetrics().getMaxAscent();
+		int y = 0;
+		int width;
+
+		y += maxAscent;
+		width = g2d.getFontMetrics().stringWidth("LEVEL");
+		g2d.drawString("LEVEL", getWidth() / 2 - width / 2, y);
+
+		y += maxAscent;
+		width = g2d.getFontMetrics().stringWidth(level);
+		g2d.drawString(level, getWidth() - width, y);
+
+		y += maxAscent * 2;
+		width = g2d.getFontMetrics().stringWidth("SCORE");
+		g2d.drawString("SCORE", getWidth() / 2 - width / 2, y);
+
+		y += maxAscent;
+		width = g2d.getFontMetrics().stringWidth(score);
+		g2d.drawString(score, getWidth() - width, y);
 	}
 
 	public void update(Observable o, Object arg) {
 		if (o == model) {
-			setText(String.format(FORMAT, model.getLevel(), model.getScore()));
+			repaint();
 		}
 	}
 }
